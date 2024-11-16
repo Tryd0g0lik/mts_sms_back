6a96014e388af3c68f3acc987443c873bbb02380
Tryd0g0lik <work80@mail.ru>
2024-11-16 14:36:27 +0700

    [master]: error of the send_sms

--- sms_postman/views.py
+++ sms_postman/views.py
@@ -8,6 +8,7 @@ from .apps import (app_sms, csrf)
 from dotenv_ import (API_KEY, API_URL, PHONE_SEND)
 from flask_wtf.csrf import CSRFError
 
+from .files import receive_pathname_js_file
 from .forms.form_message import GetFormSmsMessage
 
 
@@ -19,25 +20,33 @@ def handle_csrf_error(e):
 @csrf.exempt
 def index():
     form = GetFormSmsMessage()
+    # Below, receive the JS file name.
+    js_file_name = receive_pathname_js_file
     # csrf_token = csrf # generate_csrf()
+
+
     if request.method == 'POST' and form.validate_on_submit():
         # EXOLVE_API_KEY = generate_csrf()
-        number = request.form.get('number')
-        text = request.form.get('text')
+        number = request.form.get('mobil_number')
+        text = request.form.get('text_message')
         # api_key = request.form.get('api_key') or os.getenv('EXOLVE_API_KEY')  # Получение ключа из окружения или формы
         # api_key = EXOLVE_API_KEY
 
         if not number or not text:
             flash('Пожалуйста, заполните все поля.')
-            return render_template('index.html')
+            return render_template('index.html',
+                                   )
 
-        response = send_sms("", number, text)
+        response = send_sms(API_KEY, number, text)
         if response.status_code == 200:
             flash('SMS успешно отправлено!')
         else:
-            flash(f'Ошибка при отправке SMS: {response.json().get("message", "Неизвестная ошибка")}')
+            flash(f'Ошибка при отправке SMS: \
+{response.json().get("message", "Неизвестная ошибка")}')
     
-    return render_template('index.html', form=form )
+    return render_template('index.html',
+                           js_file_name=js_file_name,
+                           form=form )
 
 def send_sms(api_key, number, text):
     headers = {'Authorization': f'Bearer {api_key}'}
